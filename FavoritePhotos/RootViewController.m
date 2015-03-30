5162e1827687e61a4810887c1bb51e15a8c8f2f6
Ronald Hernandez <2ronald.hernandez@gmail.com>
2015-03-30 01:38:25 -0700

    created a tabbed view application, have a favorite scene, search scene and a map scene, refactored class names, added images for icon and image for splash screen

--- FavoritePhotos/RootViewController.m
+++ FavoritePhotos/RootViewController.m
@@ -6,22 +6,19 @@
 //  Copyright (c) 2015 Ron. All rights reserved.
 //
 
-#import "ViewController.h"
+#import "RootViewController.h"
 
-@interface ViewController ()
+@interface RootViewController ()
 
 @end
 
-@implementation ViewController
+@implementation RootViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
+
 
 @end
