27d434d7ff566fd4df2db51201587b8d5ef00901
Ronald Hernandez <2ronald.hernandez@gmail.com>
2015-03-30 00:17:29 -0700

    Initial Commit

---
+++ FavoritePhotos/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FavoritePhotos
+//
+//  Created by Ronald Hernandez on 3/30/15.
+//  Copyright (c) 2015 Ron. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
